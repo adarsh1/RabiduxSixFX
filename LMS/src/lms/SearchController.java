0c920c2f27fbb2b924e1092fe3716b34200e4d5d
Ambarish S P <ambarish2@e.ntu.edu.sg>
2013-03-17 01:07:41 +0800

    added to search.... introduced scroll bar

--- LMS/src/lms/SearchController.java
+++ LMS/src/lms/SearchController.java
@@ -5,17 +5,28 @@
 
 package lms;
 
-import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.RadioButton;
 
 public class SearchController extends MemberGUI implements Initializable{
   
  
+ @FXML //  fx:id="scrollpane"
+    private ScrollPane scrollpane; // Value injected by FXMLLoader   
+ 
  @FXML //  fx:id="skeyword"
-    private RadioButton skeyword; // Value injected by FXMLLoader   
+    private VBox vb; // Value injected by FXMLLoader  
+ 
+ @FXML //  fx:id="skeyword"
+    private RadioButton skeyword; // Value injected by FXMLLoader  
  
  @FXML //  fx:id="stitle"
     private RadioButton stitle; // Value injected by FXMLLoader   
@@ -26,6 +37,22 @@ public class SearchController extends MemberGUI implements Initializable{
  @FXML //  fx:id="sisbn"
     private RadioButton sisbn; // Value injected by FXMLLoader      
     
+ 
+ // Handler for Button[Button[id=null, styleClass=button]] onAction
+    public void searchbook(ActionEvent event)throws IOException{
+      scrollpane.setVisible(true);
+      vb.setVisible(true);
+      scrollpane.setContent(vb);
+      int i;
+      for(i=0;i<20;i++)
+      {  
+          Text t = new Text(10, 50, "This is a test");
+          vb.getChildren().add(t);
+      }
+      
+    }
+ 
+ 
     @Override // This method is called by the FXMLLoader when initialization is complete
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
        assert logout != null : "fx:id=\"logout\" was not injected: check your FXML file 'Welcome.fxml'.";
