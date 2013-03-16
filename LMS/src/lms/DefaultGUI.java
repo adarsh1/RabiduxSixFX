ac572d256b6d85bbaad084a8ef1fc54f98874db2
Adarsh Kanodia <adarsh1@e.ntu.edu.sg>
2013-03-16 01:40:43 -0700

    Ive Added Login and default GUI

---
+++ LMS/src/lms/DefaultGUI.java
@@ -0,0 +1,111 @@
+/**
+ * Sample Skeleton for "Welcome.fxml" Controller Class
+ * You can copy and paste this code into your favorite IDE
+ **/
+
+package lms;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+
+
+public abstract class DefaultGUI
+    implements Initializable {
+
+    @FXML //  fx:id="logout"
+    protected ImageView logout; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="name"
+    protected Text name; // Value injected by FXMLLoader
+    
+    
+    public void logout(MouseEvent event) {
+        
+   final Popup confirmLogOut = new Popup();
+
+   
+    VBox popUpVBox = new VBox(10);
+     popUpVBox.setPadding(new Insets(10));
+    
+    Text logMes=new Text(" Are you sure you\nwant to log out?");
+    logMes.setFill(Color.WHITESMOKE);
+    logMes.setFont(Font.font("Minion Pro", 16));
+    
+    HBox popUpBoxButtons = new HBox(30);
+    popUpBoxButtons.setPadding(new Insets(10));
+    
+    popUpVBox.getChildren().add(logMes);
+    
+    Button yes=new Button("Yes");
+    Button no=new Button("No");
+    //Remove Blue Borders
+    yes.setStyle("-fx-background-insets:0, 0, 1, 2;");
+    no.setStyle("-fx-background-insets:0, 0, 1, 2;");
+
+    popUpBoxButtons.getChildren().addAll(yes,no);
+    popUpVBox.getChildren().add(popUpBoxButtons);
+    //Style the pop up box
+    popUpVBox.setStyle("-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.55) , 5, 1, 0 , 1 );-fx-background-color: linear-gradient(indianred,darkred);-fx-stroke: black;-fx-stroke-width: 5;");
+    
+    confirmLogOut.getContent().add(popUpVBox);
+    
+    Node node=(Node) event.getSource();
+  Stage stage=(Stage) node.getScene().getWindow();
+  //Set the pop up
+  confirmLogOut.show(stage);
+  
+    yes.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            
+            @Override
+            public void handle(MouseEvent t){
+                confirmLogOut.hide();
+  Stage stage=(Stage) confirmLogOut.getOwnerWindow();
+  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));  
+  try
+  {Parent root = (Parent)fxmlLoader.load();     
+  Scene scene = new Scene(root);
+  stage.setScene(scene);
+  stage.show();
+  }
+  catch(IOException e){}
+            }
+        });
+     no.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            
+            @Override
+            public void handle(MouseEvent t){
+                confirmLogOut.hide();
+                  }});
+    }
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        assert logout != null : "fx:id=\"logout\" was not injected: check your FXML file ";
+        assert name != null : "fx:id=\"name\" was not injected: check your FXML file ";
+
+        // initialize your logic here: all @FXML variables will have been injected
+
+    }
+    public void setText(String string) {
+        name.setText(string); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
--- LMS/src/lms/LoginController.java
+++ LMS/src/lms/LoginController.java
@@ -1,33 +1,60 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * Sample Skeleton for "Sample.fxml" Controller Class
+ * You can copy and paste this code into your favorite IDE
+ **/
+
 package lms;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-/**
- *
- * @author Adarsh
- */
-public class LoginController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+public class LoginController
+    implements Initializable {
+
+    @FXML //  fx:id="label"
+    private Label label; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="password"
+    private PasswordField password; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="usrname"
+    private TextField usrname; // Value injected by FXMLLoader
+
+    // Handler for Button[Button[id=null, styleClass=button]] onAction
+    public void login(ActionEvent event)throws IOException{
+        Node node=(Node) event.getSource();
+  Stage stage=(Stage) node.getScene().getWindow();
+  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Welcome.fxml"));     
+
+Parent root = (Parent)fxmlLoader.load();          
+WelcomeController controller = fxmlLoader.<WelcomeController>getController();
+controller.setText(usrname.getText());
+  Scene scene = new Scene(root);
+  stage.setScene(scene);
+  stage.show();
+
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+   @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        assert label != null : "fx:id=\"label\" was not injected: check your FXML file 'Sample.fxml'.";
+        assert password != null : "fx:id=\"password\" was not injected: check your FXML file 'Sample.fxml'.";
+        assert usrname != null : "fx:id=\"usrname\" was not injected: check your FXML file 'Sample.fxml'.";
+
+        // initialize your logic here: all @FXML variables will have been injected
+
+    }
+
 }
