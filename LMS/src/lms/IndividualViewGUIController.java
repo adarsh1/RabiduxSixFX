259b86ed39d4dab0191ba02f6d41349b2f4d25d4
Adarsh Kanodia <adarsh1@e.ntu.edu.sg>
2013-03-21 10:18:06 -0700

    Search Changed IndividualGUI added

---
+++ LMS/src/lms/IndividualViewGUIController.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lms;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author Adarsh
+ */
+public class IndividualViewGUIController extends MemberGUI implements Initializable {
+
+   
+    @FXML //  fx:id="author"
+    private Text author; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="cover"
+    private ImageView cover; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="isbn"
+    private Text isbn; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="title"
+    private Text title; // Value injected by FXMLLoader
+
+
+   
+
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        assert author != null : "fx:id=\"author\" was not injected: check your FXML file 'IndividualViewGUI.fxml'.";
+        assert cover != null : "fx:id=\"cover\" was not injected: check your FXML file 'IndividualViewGUI.fxml'.";
+        assert isbn != null : "fx:id=\"isbn\" was not injected: check your FXML file 'IndividualViewGUI.fxml'.";
+        assert name != null : "fx:id=\"name\" was not injected: check your FXML file 'IndividualViewGUI.fxml'.";
+        assert title != null : "fx:id=\"title\" was not injected: check your FXML file 'IndividualViewGUI.fxml'.";
+
+        // initialize your logic here: all @FXML variables will have been injected
+
+    }
+public void setIsbn(String ISBN){
+    isbn.setText("ISBN:"+ISBN);
+}
+public void setAuthor(String a){
+    author.setText("Author:"+a);
+}
+public void setTitle(String t){
+    title.setText(t);
+}
+public void setImage(String url){
+    try
+    { cover.setImage(new Image(SearchController.class.getResourceAsStream(url)));
+    }
+    catch(Exception e){
+    }
+    }
+}
--- LMS/src/lms/SearchController.java
+++ LMS/src/lms/SearchController.java
@@ -13,8 +13,20 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
 public class SearchController extends MemberGUI implements Initializable{
   
@@ -37,17 +49,21 @@ public class SearchController extends MemberGUI implements Initializable{
  @FXML //  fx:id="sisbn"
     private RadioButton sisbn; // Value injected by FXMLLoader      
     
- 
+ private String tempT;
+ private String tempC;
+ private String tempISBN;
+ private String tempAuthor;
  // Handler for Button[Button[id=null, styleClass=button]] onAction
     public void searchbook(ActionEvent event)throws IOException{
       scrollpane.setVisible(true);
       vb.setVisible(true);
       scrollpane.setContent(vb);
+      vb.setSpacing(20);
       int i;
       for(i=0;i<50;i++)
-      {  
-          Text t = new Text(10, 50, "This is a test");
-          vb.getChildren().add(t);
+      {  Pane p=new Pane();
+            createIndividual(p);
+          vb.getChildren().add(p);
       }
       
     }
@@ -59,5 +75,50 @@ public class SearchController extends MemberGUI implements Initializable{
          assert name != null : "fx:id=\"name\" was not injected: check your FXML file 'Welcome.fxml'.";
         // initialize your logic here: all @FXML variables will have been injected
 
+    }
+
+    private void createIndividual(Pane p) {
+        p.setId("individual");
+        p.setMinSize(295, 98);
+        ImageView img=new ImageView(new Image(SearchController.class.getResourceAsStream("images/default_book_cover.jpg")));
+        img.setFitHeight(80);
+        img.setFitWidth(60);
+        img.setLayoutX(228);
+        img.setLayoutY(8);
+        img.setPreserveRatio(true);
+        Text t = new Text(14, 50, "Title Of Book 2nd Edition Champak Publishing");
+        
+        t.setWrappingWidth(210);
+       /*Text dis=new Text(9,48,"Text Description, take limited characters add ...");
+        dis.setFont(Font.font("System", FontWeight.THIN, 12));
+        dis.setWrappingWidth(215);*/
+        t.setTextOrigin(VPos.CENTER);
+        t.setTextAlignment(TextAlignment.CENTER);
+         p.getChildren().addAll(t,img);
+         tempT=t.getText();
+         tempC="images/default_book_cover.jpg";
+         tempISBN="1111111111";
+         tempAuthor="Charles Dickens";
+          p.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            
+            @Override
+            public void handle(MouseEvent e){
+                Node node=(Node) e.getSource();
+                Stage stage=(Stage) node.getScene().getWindow();
+        try{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("IndividualViewGUI.fxml"));
+        Parent root = (Parent)fxmlLoader.load();          
+      IndividualViewGUIController controller = fxmlLoader.<IndividualViewGUIController>getController();
+      controller.setTitle(tempT);
+      controller.setImage(tempC);
+      controller.setAuthor(tempAuthor);
+      controller.setIsbn(tempISBN);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        }
+        catch(IOException ei){;}
+            }
+        });
     }
    }
