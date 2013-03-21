6c387322daa967f4ae7edd6d95158e00f07c288c
Adarsh Kanodia <adarsh1@e.ntu.edu.sg>
2013-03-21 02:00:24 -0700

    Popup and Enter functionality

--- LMS/src/lms/DefaultGUI.java
+++ LMS/src/lms/DefaultGUI.java
@@ -64,7 +64,7 @@ public abstract class DefaultGUI
     popUpBoxButtons.getChildren().addAll(yes,no);
     popUpVBox.getChildren().add(popUpBoxButtons);
     //Style the pop up box
-    popUpVBox.setStyle("-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.55) , 5, 1, 0 , 1 );-fx-background-color: linear-gradient(to bottom, #191970 20% , #008080);-fx-stroke: black;-fx-stroke-width: 5;");
+    popUpVBox.setStyle("-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.55) , 5, 1, 0 , 1 );-fx-background-color: radial-gradient(focus-angle 0deg , focus-distance 0% , center 50% 50% , radius 50% , #00bfff, #000080);;-fx-stroke: black;-fx-stroke-width: 5;");
     confirmLogOut.getContent().add(popUpVBox);
     
     Node node=(Node) event.getSource();
--- LMS/src/lms/LoginController.java
+++ LMS/src/lms/LoginController.java
@@ -19,6 +19,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -36,19 +38,16 @@ public class LoginController
     
     @FXML //  fx:id="quit"
     private ImageView quit; // Value injected by FXMLLoader
-
+// Handler for Button[Button[id=null, styleClass=button]] onKeyPressed
+    public void enter(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+          Node node=(Node) event.getSource();
+          goWelcome(node);}
+    }
     // Handler for Button[Button[id=null, styleClass=button]] onAction
-    public void login(ActionEvent event)throws IOException{
+    public void login(ActionEvent event){
         Node node=(Node) event.getSource();
-  Stage stage=(Stage) node.getScene().getWindow();
-  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Welcome.fxml"));     
-
-Parent root = (Parent)fxmlLoader.load();          
-WelcomeController controller = fxmlLoader.<WelcomeController>getController();
-controller.setText(usrname.getText());
-  Scene scene = new Scene(root);
-  stage.setScene(scene);
-  stage.show();
+        goWelcome(node);     
 
     }
     // Handler for ImageView[fx:id="quit"] onMouseClicked
@@ -64,6 +63,21 @@ controller.setText(usrname.getText());
         assert quit != null : "fx:id=\"quit\" was not injected: check your FXML file 'Login.fxml'.";
         // initialize your logic here: all @FXML variables will have been injected
 
+    }
+
+    private void goWelcome(Node node){
+        Stage stage=(Stage) node.getScene().getWindow();
+        try{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Welcome.fxml"));
+        Parent root = (Parent)fxmlLoader.load();          
+      WelcomeController controller = fxmlLoader.<WelcomeController>getController();
+      controller.setText(usrname.getText());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        }
+        catch(IOException e){;}
+      
     }
 
 }
