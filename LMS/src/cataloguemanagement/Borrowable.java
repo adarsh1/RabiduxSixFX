5144ace949ff7e8913e73f1b48317b8fc79dcc15
Richard Lu <lmjrichard@gmail.com>
2013-03-24 20:54:33 +0800

    cataloguemanagement only

---
+++ LMS/src/cataloguemanagement/Borrowable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cataloguemanagement;
+
+/**
+ *
+ * @author Richard
+ */
+public interface Borrowable {
+   
+    public Boolean borrow (String memberID);
+   
+    
+}
