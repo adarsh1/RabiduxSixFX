7005244a9c131c157031b6fd1c151255fedefa37
Adarsh Kanodia <adarsh1@e.ntu.edu.sg>
2013-03-25 23:10:36 +0800

    LibrarianGUI added [Start user name with 1 for member GUI otherwise view LibrarianGUI]

---
+++ LMS/src/updatecatalogue/UpdateCatalogueController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package updatecatalogue;
+
+import baseGUI.LibrarianFXController;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Adarsh
+ */
+public class UpdateCatalogueController extends LibrarianFXController implements Initializable {
+
+     @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+       assert logout != null : "fx:id=\"logout\" was not injected: check your FXML file";
+         assert name != null : "fx:id=\"name\" was not injected: check your FXML file";
+        // initialize your logic here: all @FXML variables will have been injected
+
+    }    
+}
