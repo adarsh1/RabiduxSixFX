a6a87ab41d5373e4957734b2454d8e16be754a39
liux0047 <tracy1kobe@gmail.com>
2013-03-28 21:59:02 +0800

    modifying UI

---
+++ LMS/src/memberpage/MemberPageController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memberpage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Allen
+ */
+public class MemberPageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
