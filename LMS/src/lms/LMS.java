5d29ec4f8694396c2af0c4624a7c5b58d879c62b
liux0047 <tracy1kobe@gmail.com>
2013-03-28 23:27:19 +0800

    added shadow and fade in for login

--- LMS/src/lms/LMS.java
+++ LMS/src/lms/LMS.java
@@ -4,6 +4,7 @@
  */
 package lms;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +14,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 /**
  *
@@ -22,12 +24,15 @@ public class LMS extends Application {
     //set the inital axis of the window
     private double xOffset = 0;
     private double yOffset = 0;
+    private Scene scene;
+    private Parent root;
+    
     
     @Override
     public void start(final Stage primaryStage) throws Exception {
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        Parent root = FXMLLoader.load(getClass().getResource("/resources/xml/Login.fxml"));
-        root.setId("rootNode");
+        root = FXMLLoader.load(getClass().getResource("/resources/xml/Login.fxml"));
+        
         //enable the stage to be draggable via tow new handlers
         root.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
@@ -42,13 +47,37 @@ public class LMS extends Application {
                 primaryStage.setX(event.getScreenX() - xOffset);
                 primaryStage.setY(event.getScreenY() - yOffset);
             }
-        });
-        
-        Scene scene = new Scene(root);
-        scene.setFill(Color.TRANSPARENT);
+        });        
+               
+        //set the background to be transparent 
+        scene = new Scene(root, Color.TRANSPARENT);
         
         primaryStage.setScene(scene);
         primaryStage.show();
+        handleRootFadeIn();
+    }
+    
+    public void handleRootFadeIn(){
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(1500), root);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        fadeTransition.play();
+    }
+    
+    /*
+    @Override
+    public void start(final Stage stage) throws Exception {
+        stage.initStyle(StageStyle.TRANSPARENT); // here it is
+
+        Group rg = new Group();
+        Scene scene = new Scene(rg, 320, 240, Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+
+        Rectangle r = new Rectangle(5, 5, stage.getWidth() - 10, stage.getHeight() - 10);
+        r.setFill(Color.STEELBLUE);
+        r.setEffect(new DropShadow());
+        rg.getChildren().add(r);
     }
 
     /**
