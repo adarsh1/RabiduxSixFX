c073c323efa3bc9cec4fe46134f91a687c7722a3
Richard Lu <lmjrichard@gmail.com>
2013-03-29 17:57:57 +0800

    Mgr classes

---
+++ LMS/src/baseGUI/LibrarianFX.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baseGUI;
+
+/**
+ *
+ * @author Richard
+ */
+public class LibrarianFX {
+    
+}
---
+++ LMS/src/returnbook/ReturnMgr.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package returnbook;
+
+/**
+ *
+ * @author Richard
+ */
+public class ReturnMgr {
+    
+}
