e572c18e68285f5731e94b5cee2ba2942c69ab3d
liux0047 <tracy1kobe@gmail.com>
2013-03-29 15:12:43 +0800

    rename baseFXController

--- LMS/src/baseGUI/BaseFXController.java
+++ LMS/src/baseGUI/BaseFXController.java
@@ -5,6 +5,10 @@ import globalcontroller.MainController;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -24,9 +28,10 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
-public abstract class BaseGUIController implements Initializable {
+public abstract class BaseFXController implements Initializable {
 
     @FXML //  fx:id="logout"
     protected ImageView logout; // Value injected by FXMLLoader
@@ -36,6 +41,7 @@ public abstract class BaseGUIController implements Initializable {
     
     //main controller
     private MainController MC;
+    
     
     public void logout(MouseEvent event) {
         
@@ -106,7 +112,7 @@ public abstract class BaseGUIController implements Initializable {
             Stage stage=(Stage) node.getScene().getWindow();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(resourceURL)); 
             Parent root = (Parent)fxmlLoader.load(); 
-            BaseGUIController FXController = fxmlLoader.<BaseGUIController>getController();
+            BaseFXController FXController = fxmlLoader.<BaseFXController>getController();
             FXController.setMC(MC);            
             Scene scene = new Scene(root);
             stage.setScene(scene);
@@ -130,4 +136,55 @@ public abstract class BaseGUIController implements Initializable {
     public void setMC(MainController MC) {
         this.MC = MC;
     }
+    
+    //show element with Metro style animation
+    //static funtion to be called without knowing the Pane
+    public static void handleOnShowAnimation(Node node, int millis, double offset){
+        //fade in
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(millis), node);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        //Scale in
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(millis / 2), node);
+        scaleTransition.setFromX(0.0);
+        scaleTransition.setToX(1.0);
+        scaleTransition.setCycleCount(1);
+        scaleTransition.setAutoReverse(false);
+        //transition in
+        TranslateTransition translateTransition = new TranslateTransition(Duration.millis(millis * 2), node);
+        translateTransition.setFromX( offset * (-1));
+        translateTransition.setToX(0.0);
+        translateTransition.setCycleCount(1);
+        translateTransition.setAutoReverse(false);
+        //parallel animation
+        ParallelTransition parallelTransition = new ParallelTransition(node, fadeTransition, scaleTransition, translateTransition);
+        parallelTransition.play();
+    }
+    
+    //apply fade animation
+    public void handleNodeFadeTransition(Node node, int millis, double fromValue, double toValue){
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(millis), node);
+        fadeTransition.setFromValue(fromValue);
+        fadeTransition.setToValue(toValue);
+        fadeTransition.play();
+    }
+   
+    //overloading handleNodeFadeTransistion with default from value and to value
+    public void handleNodeFadeTransition(Node node, int millis){
+        handleNodeFadeTransition(node, millis, 0.0, 1.0);
+    }
+   
+    //apply scale transition to a node element
+    public void handleNodeScaleTransition (Node node, int millis, double fromValue, double toValue ){        
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(millis), node);
+        scaleTransition.setFromX(fromValue);
+        scaleTransition.setToX(toValue);
+        scaleTransition.setCycleCount(1);
+        scaleTransition.setAutoReverse(false);
+        scaleTransition.play();
+    }
+    //overloading handleNodeScaleTransition with default from value and to value
+     public void handleNodeScaleTransition (Node node, int millis){
+         handleNodeScaleTransition(node, millis, 0.0, 0.1);
+     }
 }
