89e5c39052e997f5866a222e7539bdd2ceb521f1
liux0047 <tracy1kobe@gmail.com>
2013-03-30 12:38:35 +0800

    added testBorrow

--- LMS/src/lms/LMS.java
+++ LMS/src/lms/LMS.java
@@ -61,23 +61,7 @@ public class LMS extends Application {
         BaseFXController.handleOnShowAnimation(root, 500, offset);
     }    
     
-    
-    
-    /*
-    @Override
-    public void start(final Stage stage) throws Exception {
-        stage.initStyle(StageStyle.TRANSPARENT); // here it is
-
-        Group rg = new Group();
-        Scene scene = new Scene(rg, 320, 240, Color.TRANSPARENT);
-        stage.setScene(scene);
-        stage.show();
-
-        Rectangle r = new Rectangle(5, 5, stage.getWidth() - 10, stage.getHeight() - 10);
-        r.setFill(Color.STEELBLUE);
-        r.setEffect(new DropShadow());
-        rg.getChildren().add(r);
-    }
+   
 
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
