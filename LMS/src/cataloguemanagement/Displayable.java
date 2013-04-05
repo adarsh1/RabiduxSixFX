617cf8dfd4cd09b6ac83a64194eb4caf792c152f
liux0047 <tracy1kobe@gmail.com>
2013-04-06 00:11:09 +0800

    refactors

--- LMS/src/cataloguemanagement/Displayable.java
+++ LMS/src/cataloguemanagement/Displayable.java
@@ -12,7 +12,7 @@ import java.util.Calendar;
  */
 public interface Displayable {
     /**
-     *
+     * get Strings for various attributes that client class may use to display
      */
     public abstract String getItemID();
     public abstract String getTitle();
@@ -21,5 +21,6 @@ public interface Displayable {
     public abstract Calendar getPublishDate();
     public abstract String getISBN();
     public abstract String getGenre();
+    public abstract String getLocation();
     
 }
