53e05f32ae281fe7233ea2df2b2ffe61c718e087
Ambarish S P <ambarish2@e.ntu.edu.sg>
2013-04-16 22:33:48 +0800

    mgr for it

--- LMS/src/resetfine/ResetFineMgr.java
+++ LMS/src/resetfine/ResetFineMgr.java
@@ -4,10 +4,59 @@
  */
 package resetfine;
 
+import cataloguemanagement.Borrowable;
+import cataloguemanagement.CatalogueItem;
+import cataloguemanagement.PastTransaction;
+import exception.NotEligibleToBorrowOrReserveException;
+import exception.TypeMismatchException;
+import java.sql.SQLException;
+import usermanagement.Member;
+
 /**
  *
  * @author Richard
  */
 public class ResetFineMgr {
+ 
+     
+    private Member currentMember;
     
+    //a member object required to construct
+    public ResetFineMgr(Member currentMember){
+        this.currentMember = currentMember;
+    }
+    public ResetFineMgr(){
+        //empty constructor
+    }
+    
+
+    /**
+     * @return the currentMember
+     */
+    public Member getCurrentMember() {
+        return currentMember;
+    }
+
+    /**
+     * @param currentMember the currentMember to set
+     */
+    public void setCurrentMember(Member currentMember) {
+        this.currentMember = currentMember;
+    }
+    
+    
+    public void setMember(String userID)
+    {
+        //get member from datastore 
+    }
+
+    public String getUserName()
+    {
+        return currentMember.getUsername();
+    }
+    
+    public String getUserFine()
+    {
+        return (currentMember.getFineAmount() + "");
+    }
 }
