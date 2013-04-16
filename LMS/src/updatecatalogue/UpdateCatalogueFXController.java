5d0d50278636c1b770141bd5a57c4db1d6ea8993
liux0047 <tracy1kobe@gmail.com>
2013-04-16 14:28:53 +0800

    updates

--- LMS/src/updatecatalogue/UpdateCatalogueFXController.java
+++ LMS/src/updatecatalogue/UpdateCatalogueFXController.java
@@ -1,6 +1,8 @@
 
 package updatecatalogue;
 
+import baseGUI.BaseFXController;
+import globalcontrol.ModelController;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -9,7 +11,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -20,83 +21,63 @@ import javafx.scene.layout.AnchorPane;
  * @author Adarsh
  */
 
-public class UpdateCatalogueFXController
+public class UpdateCatalogueFXController extends BaseFXController
     implements Initializable {
+
 
     @FXML //  fx:id="DVDType"
     private RadioButton DVDType; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="ItemTitle"
-    private Label ItemTitle; // Value injected by FXMLLoader
-
     @FXML //  fx:id="bookType"
     private RadioButton bookType; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="confirmButton"
-    private Button confirmButton; // Value injected by FXMLLoader
 
     @FXML //  fx:id="contentPane"
     private AnchorPane contentPane; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="deleteItemPane"
-    private Tab deleteItemPane; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemAuthor"
+    private Label newItemAuthor; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="hideMessage"
-    private Button hideMessage; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemAuthorField"
+    private TextField newItemAuthorField; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="itemAuthor"
-    private Label itemAuthor; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemConfirm"
+    private Button newItemConfirm; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="itemAuthorField"
-    private TextField itemAuthorField; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemDescription"
+    private Label newItemDescription; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="itemDescription"
-    private Label itemDescription; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemDescriptionField"
+    private TextArea newItemDescriptionField; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="itemDescriptionField"
-    private TextArea itemDescriptionField; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemGenre"
+    private Label newItemGenre; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="itemGenre"
-    private Label itemGenre; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemGenreField"
+    private TextField newItemGenreField; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="itemGenreField"
-    private TextField itemGenreField; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemISBN"
+    private Label newItemISBN; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="itemISBN"
-    private Label itemISBN; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="itemISBNField"
-    private TextField itemISBNField; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="itemPublishDate"
-    private Label itemPublishDate; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="itemPublishDateField"
-    private TextField itemPublishDateField; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="itemTitleField"
-    private TextField itemTitleField; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="itemType"
-    private Label itemType; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="messageHeader"
-    private Label messageHeader; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="messageHolderPane"
-    private AnchorPane messageHolderPane; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="messagePane"
-    private AnchorPane messagePane; // Value injected by FXMLLoader
-
-    @FXML //  fx:id="messageText"
-    private Label messageText; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemISBNField"
+    private TextField newItemISBNField; // Value injected by FXMLLoader
 
     @FXML //  fx:id="newItemPane"
     private AnchorPane newItemPane; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="updateItemPane"
-    private AnchorPane updateItemPane; // Value injected by FXMLLoader
+    @FXML //  fx:id="newItemPublishDate"
+    private Label newItemPublishDate; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="newItemPublishDateField"
+    private TextField newItemPublishDateField; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="newItemTitle"
+    private Label newItemTitle; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="newItemTitleField"
+    private TextField newItemTitleField; // Value injected by FXMLLoader
+
+    @FXML //  fx:id="newItemType"
+    private Label newItemType; // Value injected by FXMLLoader
 
 
     // Handler for Button[fx:id="hideMessage"] onAction
@@ -106,34 +87,42 @@ public class UpdateCatalogueFXController
 
     @Override // This method is called by the FXMLLoader when initialization is complete
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-        assert DVDType != null : "fx:id=\"DVDType\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert ItemTitle != null : "fx:id=\"ItemTitle\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert bookType != null : "fx:id=\"bookType\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert confirmButton != null : "fx:id=\"confirmButton\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert contentPane != null : "fx:id=\"contentPane\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert deleteItemPane != null : "fx:id=\"deleteItemPane\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert hideMessage != null : "fx:id=\"hideMessage\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemAuthor != null : "fx:id=\"itemAuthor\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemAuthorField != null : "fx:id=\"itemAuthorField\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemDescription != null : "fx:id=\"itemDescription\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemDescriptionField != null : "fx:id=\"itemDescriptionField\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemGenre != null : "fx:id=\"itemGenre\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemGenreField != null : "fx:id=\"itemGenreField\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemISBN != null : "fx:id=\"itemISBN\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemISBNField != null : "fx:id=\"itemISBNField\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemPublishDate != null : "fx:id=\"itemPublishDate\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemPublishDateField != null : "fx:id=\"itemPublishDateField\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemTitleField != null : "fx:id=\"itemTitleField\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert itemType != null : "fx:id=\"itemType\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert messageHeader != null : "fx:id=\"messageHeader\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert messageHolderPane != null : "fx:id=\"messageHolderPane\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert messagePane != null : "fx:id=\"messagePane\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert messageText != null : "fx:id=\"messageText\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert newItemPane != null : "fx:id=\"newItemPane\" was not injected: check your FXML file 'UpdateItem.fxml'.";
-        assert updateItemPane != null : "fx:id=\"updateItemPane\" was not injected: check your FXML file 'UpdateItem.fxml'.";
+        assert DVDType != null : "fx:id=\"DVDType\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert bookType != null : "fx:id=\"bookType\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert contentPane != null : "fx:id=\"contentPane\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert hideMessage != null : "fx:id=\"hideMessage\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert messageHeader != null : "fx:id=\"messageHeader\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert messageHolderPane != null : "fx:id=\"messageHolderPane\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert messagePane != null : "fx:id=\"messagePane\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert messageText != null : "fx:id=\"messageText\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemAuthor != null : "fx:id=\"newItemAuthor\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemAuthorField != null : "fx:id=\"newItemAuthorField\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemConfirm != null : "fx:id=\"newItemConfirm\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemDescription != null : "fx:id=\"newItemDescription\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemDescriptionField != null : "fx:id=\"newItemDescriptionField\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemGenre != null : "fx:id=\"newItemGenre\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemGenreField != null : "fx:id=\"newItemGenreField\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemISBN != null : "fx:id=\"newItemISBN\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemISBNField != null : "fx:id=\"newItemISBNField\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemPane != null : "fx:id=\"newItemPane\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemPublishDate != null : "fx:id=\"newItemPublishDate\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemPublishDateField != null : "fx:id=\"newItemPublishDateField\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemTitle != null : "fx:id=\"newItemTitle\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemTitleField != null : "fx:id=\"newItemTitleField\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
+        assert newItemType != null : "fx:id=\"newItemType\" was not injected: check your FXML file 'UpdateCatalogue.fxml'.";
 
         // initialize your logic here: all @FXML variables will have been injected
 
+    }
+
+    @Override
+    public void setInitialData(ModelController modelController) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void playOnShowAnimation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
