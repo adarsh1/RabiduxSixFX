3b5f9027a5b17c52a30bd06c341b8d09adc81698
Ambarish S P <ambarish2@e.ntu.edu.sg>
2013-04-19 02:59:09 +0800

    holdings tests!

--- LMS/test/Holdings/HoldingsMgrTest.java
+++ LMS/test/Holdings/HoldingsMgrTest.java
@@ -6,14 +6,19 @@ package Holdings;
 
 import cataloguemanagement.CurrentHolding;
 import cataloguemanagement.ReservedCopy;
+import exception.NotEligibleToBorrowOrReserveException;
+import exception.UserSuspendedException;
+import factory.SystemConfig;
 import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import usermanagement.Member;
+import usermanagement.User;
 
 /**
  *
@@ -26,6 +31,8 @@ public class HoldingsMgrTest {
     
     @BeforeClass
     public static void setUpClass() {
+        SystemConfig instance = SystemConfig.getInstance();
+        instance.useMySQLDB();
     }
     
     @AfterClass
@@ -44,146 +51,99 @@ public class HoldingsMgrTest {
      * Test of extend method, of class HoldingsMgr.
      */
     @Test
-    public void testExtend() throws Exception {
-        System.out.println("extend");
+    public void testExtendAboveLimit() throws Exception {
+        System.out.println("extendabovelimit");
         int i = 0;
         HoldingsMgr instance = new HoldingsMgr();
-        instance.extend(i);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        //Assume that tester is a member who has borrowed a single book and has extended it thrice.
+        
+        Member tester = (Member) User.getUser("1111111110");
+        instance.setCurrentMember(tester);
+        instance.setCurrentHoldings(tester.getCurrentHoldingItems());
+        try{
+            instance.extend(i);
+            fail("The test case is a prototype.");
+        }
+        catch(Exception e)
+        {
+          Assert.assertTrue("Incorrect Exception",e instanceof NotEligibleToBorrowOrReserveException);
+        }
     }
 
+    
+   
     /**
-     * Test of cancelReservation method, of class HoldingsMgr.
+     * Test of extend method, of class HoldingsMgr.
      */
     @Test
-    public void testCancelReservation() throws Exception {
-        System.out.println("cancelReservation");
+    public void testExtendBelowLimit() throws Exception {
+        System.out.println("extendbelowlimit");
         int i = 0;
         HoldingsMgr instance = new HoldingsMgr();
-        instance.cancelReservation(i);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        //Assume that tester is a member who has borrowed a single book and has extended it thrice.
+        
+        Member tester = (Member) User.getUser("1111111112");
+        instance.setCurrentMember(tester);
+        instance.setCurrentHoldings(tester.getCurrentHoldingItems());
+        try{
+            instance.extend(i);
+            System.out.println("Passed");
+        }
+        catch(Exception e)
+        {
+          fail("The test case is a prototype.");
+        }
     }
-
+    
     /**
-     * Test of createItem method, of class HoldingsMgr.
+     * Test of extend method, of class HoldingsMgr.
      */
     @Test
-    public void testCreateItem() throws Exception {
-        System.out.println("createItem");
+    public void testExtendBookReserved() throws Exception {
+        System.out.println("extendreserved");
+        int i = 0;
         HoldingsMgr instance = new HoldingsMgr();
-        instance.createItem();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        //Assume that tester is a member who has borrowed a single book and that book has now been reserved by another member
+        
+        Member tester = (Member) User.getUser("1111111113");
+        instance.setCurrentMember(tester);
+        instance.setCurrentHoldings(tester.getCurrentHoldingItems());
+        try{
+            instance.extend(i);
+            fail("The test case is a prototype.");
+        }
+        catch(Exception e)
+        {
+          System.out.println("Passed");
+        }
     }
-
-    /**
-     * Test of getCurrentMember method, of class HoldingsMgr.
+    
+        /**
+     * Test of extend method, of class HoldingsMgr.
      */
     @Test
-    public void testGetCurrentMember() {
-        System.out.println("getCurrentMember");
+    public void testExtendSuspended() throws Exception {
+        System.out.println("extendsuspended");
+        int i = 0;
         HoldingsMgr instance = new HoldingsMgr();
-        Member expResult = null;
-        Member result = instance.getCurrentMember();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        //Assume that tester is a member who has borrowed a single book and now has a fine.
+        
+        Member tester = (Member) User.getUser("1111111114");
+        instance.setCurrentMember(tester);
+        instance.setCurrentHoldings(tester.getCurrentHoldingItems());
+        try{
+            instance.extend(i);
+            fail("The test case is a prototype.");
+        }
+        catch(Exception e)
+        {
+          Assert.assertTrue("Incorrect Exception",e instanceof UserSuspendedException);
+        }
     }
 
-    /**
-     * Test of setCurrentMember method, of class HoldingsMgr.
-     */
-    @Test
-    public void testSetCurrentMember() {
-        System.out.println("setCurrentMember");
-        Member currentMember = null;
-        HoldingsMgr instance = new HoldingsMgr();
-        instance.setCurrentMember(currentMember);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getReservedCopies method, of class HoldingsMgr.
-     */
-    @Test
-    public void testGetReservedCopies() {
-        System.out.println("getReservedCopies");
-        HoldingsMgr instance = new HoldingsMgr();
-        ArrayList expResult = null;
-        ArrayList result = instance.getReservedCopies();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setReservedCopies method, of class HoldingsMgr.
-     */
-    @Test
-    public void testSetReservedCopies() {
-        System.out.println("setReservedCopies");
-        ArrayList<ReservedCopy> item = null;
-        HoldingsMgr instance = new HoldingsMgr();
-        instance.setReservedCopies(item);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getCurrentHoldings method, of class HoldingsMgr.
-     */
-    @Test
-    public void testGetCurrentHoldings() {
-        System.out.println("getCurrentHoldings");
-        HoldingsMgr instance = new HoldingsMgr();
-        ArrayList expResult = null;
-        ArrayList result = instance.getCurrentHoldings();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setCurrentHoldings method, of class HoldingsMgr.
-     */
-    @Test
-    public void testSetCurrentHoldings() {
-        System.out.println("setCurrentHoldings");
-        ArrayList<CurrentHolding> item = null;
-        HoldingsMgr instance = new HoldingsMgr();
-        instance.setCurrentHoldings(item);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getNoOfCurrentHoldings method, of class HoldingsMgr.
-     */
-    @Test
-    public void testGetNoOfCurrentHoldings() {
-        System.out.println("getNoOfCurrentHoldings");
-        HoldingsMgr instance = new HoldingsMgr();
-        int expResult = 0;
-        int result = instance.getNoOfCurrentHoldings();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getNoOfReservedCopies method, of class HoldingsMgr.
-     */
-    @Test
-    public void testGetNoOfReservedCopies() {
-        System.out.println("getNoOfReservedCopies");
-        HoldingsMgr instance = new HoldingsMgr();
-        int expResult = 0;
-        int result = instance.getNoOfReservedCopies();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    
 }
