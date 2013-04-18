25efc94d7934585ca46318e5c2ec3629a82e59f3
liux0047 <tracy1kobe@gmail.com>
2013-04-18 12:33:59 +0800

    cleaning up

--- LMS/test/RootSuite.java
+++ LMS/test/RootSuite.java
@@ -15,7 +15,7 @@ import org.junit.runners.Suite;
  * @author Allen
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({searchbook.SearchbookSuite.class, history.HistorySuite.class, Holdings.HoldingsSuite.class, borrowbook.BorrowbookSuite.class, lms.LmsSuite.class, exception.ExceptionSuite.class, resources.ResourcesSuite.class, returnbook.ReturnbookSuite.class, datamanagement.DatamanagementSuite.class, baseGUI.BaseGUISuite.class, factory.FactorySuite.class, cataloguemanagement.CataloguemanagementSuite.class, test.TestSuite.class, usermanagement.UsermanagementSuite.class, resetfine.ResetfineSuite.class, updatecatalogue.UpdatecatalogueSuite.class, memberpage.MemberpageSuite.class, globalcontrol.GlobalcontrolSuite.class, librarianpage.LibrarianpageSuite.class, login.LoginSuite.class})
+@Suite.SuiteClasses({searchbook.SearchbookSuite.class, history.HistorySuite.class, Holdings.HoldingsSuite.class, borrowbook.BorrowbookSuite.class, lms.LmsSuite.class, exception.ExceptionSuite.class, resources.ResourcesSuite.class, returnbook.ReturnbookSuite.class, datamanagement.DatamanagementSuite.class, baseGUI.BaseGUISuite.class, factory.FactorySuite.class, cataloguemanagement.CataloguemanagementSuite.class, usermanagement.UsermanagementSuite.class, resetfine.ResetfineSuite.class, updatecatalogue.UpdatecatalogueSuite.class, memberpage.MemberpageSuite.class, globalcontrol.GlobalcontrolSuite.class, librarianpage.LibrarianpageSuite.class, login.LoginSuite.class})
 public class RootSuite {
 
     @BeforeClass
