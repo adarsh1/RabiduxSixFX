ea4965aef52a0bdd4a0abb5cceb6dc1c0702ae17
Adarsh Kanodia <adarsh1@e.ntu.edu.sg>
2013-04-18 20:37:56 +0800

    Revert "fix"
    This reverts commit d6a46abc197fc5fbe07be6955b20ad45d6bab2ec.

--- LMS/src/searchbook/SearchFXController.java
+++ LMS/src/searchbook/SearchFXController.java
@@ -122,6 +122,7 @@ public class SearchFXController extends BaseFXController implements Initializabl
      */
     // Handler for Button[Button[id=null, styleClass=button]] onAction
     public void handlesearchbutton(ActionEvent event)throws IOException{
+        if(!searchButton.isDisabled())
         getDetails();
     }
     
@@ -193,6 +194,10 @@ public class SearchFXController extends BaseFXController implements Initializabl
         updateSearchButton();
     }
 
+    @FXML
+    void searchButtonPress(ActionEvent event) {
+        getDetails();
+    }
     /**
      * Initializes the Scroll Pane which displays all the search results.
      */
